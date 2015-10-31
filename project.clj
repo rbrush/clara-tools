4d824e0b2d67514fe84fc9416bf3eaa73ec60750
Ryan Brush <rbrush@gmail.com>
2015-10-31 08:11:54 -0500

    lein-release plugin: preparing 0.1.1 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.toomuchcode/clara-tools "0.1.1-SNAPSHOT"
+(defproject org.toomuchcode/clara-tools "0.1.1"
 
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.145"]
