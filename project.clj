6988d6fc60f9577f80a8ef29de50e833fd27b798
Ryan Brush <rbrush@gmail.com>
2015-10-31 08:03:01 -0500

    lein-release plugin: bumped version from 0.1.0 to 0.1.1-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.toomuchcode/clara-tools "0.1.0"
+(defproject org.toomuchcode/clara-tools "0.1.1-SNAPSHOT"
 
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.145"]
